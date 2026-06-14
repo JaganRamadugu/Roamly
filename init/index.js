c103ca479e03e15aa7b0e26ceafc71663aed084b
JaganRamadugu <rjaganbusiness@gmail.com>
2026-06-14 17:42:27 +0530

    new data initialized

--- init/index.js
+++ init/index.js
@@ -4,23 +4,16 @@ const Listings = require("../models/listing.js");
 
 const mongo_url = "mongodb://127.0.0.1:27017/roamly";
 
-main()
-  .then(() => {
-    console.log("Connected to DB");
-  })
-  .catch(() => {
-    console.log(err);
-  });
 async function main() {
   await mongoose.connect(mongo_url);
 }
 
+main()
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
+
 const initDB = async () => {
   await Listings.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "6a22b2d02d518da471a0d93d",
-  }));
   await Listings.insertMany(initData.data);
   console.log("data was initialized");
 };
